11e8667b72e49c171e668ed2650c4ad8c9534d1c
ncernst <ncernst@gmail.com>
2016-12-29 20:55:47 +0000

    Added get_entry, edit_entry, delete_entry, and partially implemented page view limit modification.

--- blog/views.py
+++ blog/views.py
@@ -5,18 +5,29 @@ from .database import session, Entry
 
 PAGINATE_BY = 10
 
+
 @app.route("/")
 @app.route("/page/<int:page>")
 def entries(page=1):
     #zero-indexed page
-    page_index = page -1
-    
+    page_index = page-1
+
     count = session.query(Entry).count()
     
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
     
-    total_pages = (count-1) // PAGINATE_BY + 1
+    if request.args.get('limit') != None and request.args.get('limit').isdigit():
+        page_limit = int(request.args.get('limit')) 
+        if page_limit == 0:
+            page_limit = PAGINATE_BY
+        elif page_limit > 100:
+            page_limit = 100
+    else:   
+        page_limit = PAGINATE_BY
+    
+    start = page_index * page_limit
+    end = start + page_limit
+    
+    total_pages = (count-1) // page_limit + 1
     has_next = page_index < total_pages-1
     has_prev = page_index > 0
     
@@ -33,7 +44,7 @@ def entries(page=1):
         page=page,
         total_pages=total_pages
     )
-    
+
 @app.route("/entry/add", methods=["GET"])
 def add_entry_get():
     return render_template("add_entry.html")
@@ -46,4 +57,46 @@ def add_entry_post():
     )
     session.add(entry)
     session.commit()
+    return redirect(url_for('entries', page = 1))
+    
+@app.route("/entry/<int:id>")
+def go_to_entry(id):
+    
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    
+    return render_template("go_to_entry.html", entry = entry)
+    
+@app.route("/entry/edit/<int:id>", methods=["GET"])
+def edit_entry_get(id):
+    
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    return render_template("edit_entry.html", entry = entry)
+    
+@app.route("/entry/edit/<int:id>", methods=["POST"])
+def edit_entry_post(id):
+    
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    
+    entry.title = request.form["title"]
+    entry.content=request.form["content"]
+
+    session.commit()
+    
+    return redirect(url_for('go_to_entry', id = id))
+
+@app.route("/entry/delete/<int:id>", methods=["GET"])
+def delete_entry_get(id):
+    
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    
+    return render_template('delete_entry.html', entry = entry)
+    
+@app.route("/entry/delete/<int:id>", methods=["POST"])
+def delete_entry(id):
+    
+    entry = session.query(Entry).filter(Entry.id == id).first()
+    session.delete(entry)
+    session.commit()
+    
     return redirect(url_for('entries'))
+    
